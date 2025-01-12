5b1916acf48044679ac627aab23db58e9e73d212
alon.dotan <alon.dotan@starkware.co>
2025-01-12 19:55:00 +0200

    add batch support

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from os import name
 import time
 import random
 import json
@@ -8,6 +9,7 @@ from kubernetes import client, config, watch
 
 config.load_incluster_config()
 v1 = client.CoreV1Api()
+batch1 = client.BatchV1Api()
 
 scheduler_name = "simple-python-scheduler"
 NAMESPACE = "test-ns"
@@ -44,10 +46,31 @@ def preemption(priority):
     return None
 
 
-def scheduler(name, priority, node, namespace=NAMESPACE):
+def get_free_slots(priority):
+    slots = 0
+    for pod in get_running_pods():
+        try:
+            pod_priority = pod.metadata.annotations["priority"]
+        except:
+            slots += 1
+        if int(pod_priority) < int(priority):
+            slots += 1
+    return slots
+
+
+def scheduler(object, priority, node, namespace=NAMESPACE):
+    try:
+        job_name = object.metadata.labels["job-name"]
+        job = batch1.read_namespaced_job(name=job_name, namespace=namespace)
+        if job.spec.parallelism > get_free_slots(priority):
+            return None
+    except:
+        pass
+
     while len(v1.list_node().items) <= len(get_running_pods()):
         preemption(priority)
 
+    name = object.metadata.name
     target = client.V1ObjectReference(kind="Node", api_version="v1", name=node)
     meta = client.V1ObjectMeta(name=name)
     body = client.V1Binding(target=target, metadata=meta)
@@ -69,7 +92,7 @@ def main():
                 except TypeError:
                     priority = 1000
                 res = scheduler(
-                    name=event["object"].metadata.name,
+                    object=event["object"],
                     priority=priority,
                     node=random.choice(nodes_available()),
                 )
